97b582f5d990e92632d752cd93da9dffefed3222
builderbot <135554020+juniorvish@users.noreply.github.com>
2023-10-21 01:11:54 +0530

    Create tests/__init__.py

---
+++ tests/__init__.py
@@ -0,0 +1,4 @@
+# The __init__.py file in the tests directory is usually empty or contains imports for test modules. 
+# Since no specific imports or configurations are mentioned in the prompt, this file will be left empty.
+
+""
